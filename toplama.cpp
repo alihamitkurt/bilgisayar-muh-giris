b74b894776d7824dbb93314a504d9d3ffcce7aff
Kudrethan <kudrethanozbudak@gmail.com>
2025-10-31 16:53:05 +0300

    Toplama

---
+++ toplama.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int sayi1, sayi2, toplam;
+
+    cout << "Birinci sayiyi girin: ";
+    cin >> sayi1;
+
+    cout << "Ikinci sayiyi girin: ";
+    cin >> sayi2;
+    
+    toplam = sayi1 + sayi2;
+
+    cout << "Toplam: " << toplam << endl;
+
+    return 0;
+}
