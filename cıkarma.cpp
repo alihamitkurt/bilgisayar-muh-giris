1f5056996755b44952f7a21cba10ab3a6dae373a
alihamitkurt <alihamitkurt07@gmail.com>
2025-10-31 16:56:50 +0300

    cıkarma

---
+++ cıkarma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sayi1, sayi2;
+    
+    cout << "Birinci sayiyi girin: ";
+    cin >> sayi1;
+    
+    cout << "İkinci sayiyi girin: ";
+    cin >> sayi2;
+    
+    
+    double sonuc = sayi1 - sayi2;
+    
+    cout << "Sonuç: " << sonuc << endl;
+    
+    return 0;
+}
